5a6b9347e298c7d54ed322646640089caf25e4e6
Mitja Nikolaus <mitja.nikolaus@posteo.de>
2022-09-19 15:12:48 +0200

    Update grid search

--- sbatch_scripts/grid_search.sh
+++ sbatch_scripts/grid_search.sh
@@ -8,7 +8,7 @@ for entropy_coeff in 0.1 0.3 0.5 0.7
 do
 	#Baseline
 	output_folder=out/train_baseline_${job_id}.out
-	baseline_args="--sender-entropy-coeff=$entropy_coeff --receiver-entropy-coeff=$entropy_coeff --num-features 4 --num-values 4 --vocab-size 100 --max-len 10"
+	baseline_args="--sender-entropy-coeff=$entropy_coeff --receiver-entropy-coeff=$entropy_coeff --num-attributes 2 --num-values 100 --vocab-size 100 --max-len 10"
 	#--log-topsim-on-validation --log-posdis-on-validation --log-bosdis-on-validation
 	sbatch --job-name "train_baseline" --ntasks 8 --time 8:00:00 --output ${output_folder} run.sh $baseline_args
     	# CRs
