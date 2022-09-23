bae101018a331211b54e2c6f58055d23185acbcc
Mitja Nikolaus <mitja.nikolaus@posteo.de>
2022-09-23 16:29:14 +0200

    Fix eval

--- eval.py
+++ eval.py
@@ -1,8 +1,8 @@
 import argparse
+import os
 from argparse import Namespace
 
 import torch
-import yaml
 import pytorch_lightning as pl
 from pytorch_lightning import seed_everything
 
@@ -10,42 +10,48 @@ from data import SignalingGameDataModule
 from model import SignalingGameModule
 
 
+def find_best_checkpoint(run_folder):
+    checkpoints_folder = os.path.join(run_folder, "checkpoints")
+
+    ckpt_files = os.listdir(checkpoints_folder)
+    best_checkpoints = [ckpt for ckpt in ckpt_files if not ckpt == "last.ckpt"]
+    # TODO for now returning the first checkpoint found
+    # accs = [int(filename[6:-5]) for filename in ckpt_files]
+    # print(max(epochs))
+    if len(best_checkpoints) == 0:
+        raise FileNotFoundError
+    assert len(best_checkpoints) == 1
+    return os.path.join(checkpoints_folder, best_checkpoints[-1])
+
+
 def run(args):
-    with open(args.hparams) as f:
-        config = yaml.load(f, Loader=yaml.FullLoader)
+    checkpoint_path = find_best_checkpoint(args.run_dir)
+    print("Loading checkpoint: " + checkpoint_path)
+    model = SignalingGameModule.load_from_checkpoint(checkpoint_path)
+    config = model.params
 
     seed_everything(config["seed"], workers=True)
 
-    checkpoint = args.checkpoint
-    print("Loading checkpoint: " + checkpoint)
-    model = SignalingGameModule.load_from_checkpoint(checkpoint, **config)
-
-    model.params.log_topsim_on_validation = True
-    model.params.log_posdis_on_validation = True
-    model.params.log_bosdis_on_validation = True
-    model.params.log_entropy_on_validation = True
-
-    datamodule = SignalingGameDataModule(num_attributes=config["model"]["num_attributes"],
-                                         num_values=config["model"]["num_values"],
-                                         max_num_objects=config["data"]["max_num_objects"],
-                                         test_set_size=config["data"]["test_set_size"],
-                                         batch_size=config["data"]["batch_size"],
-                                         num_workers=config["data"]["num_workers"])
+    datamodule = SignalingGameDataModule(num_attributes=config.num_attributes,
+                                         num_values=config.num_values,
+                                         max_num_objects=config.max_num_objects,
+                                         test_set_size=config.test_set_size,
+                                         batch_size=config.batch_size,
+                                         num_workers=config.num_workers)
 
     if torch.cuda.is_available():
-        config["trainer"]["gpus"] = 1
+        config["gpus"] = 1
     else:
-        config["trainer"]["gpus"] = 0
+        config["gpus"] = 0
 
-    trainer = pl.Trainer.from_argparse_args(Namespace(**config["trainer"]))
+    trainer = pl.Trainer.from_argparse_args(config)
 
     trainer.validate(model, datamodule=datamodule, verbose=True)
 
 
 def get_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--checkpoint", type=str, required=True)
-    parser.add_argument("--hparams", type=str, required=True)
+    parser.add_argument("--run-dir", type=str, required=True)
 
     return parser.parse_args()
 
