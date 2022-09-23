7242649b0aa3ddaff2b4b82e8188e0042dc1e1c0
Mitja Nikolaus <mitja.nikolaus@posteo.de>
2022-09-23 23:56:37 +0200

    Update grid search

--- sbatch_scripts/grid_search.sh
+++ sbatch_scripts/grid_search.sh
@@ -4,18 +4,20 @@ set -e
 
 job_id=0
 
-for entropy_coeff in 0.01 0.05 0.1 0.3 0.5 0.7 0.9
+for entropy_coeff in 0.05 0.1 0.3 0.5 0.7 #0.9 0.01
 do
-	for seed in 1 2 3
+	for seed in 1 2 3 4 5
 	do
 		#Baseline
 		output_folder=out/train_baseline_${job_id}.out
-		baseline_args="--sender-entropy-coeff=$entropy_coeff --receiver-entropy-coeff=$entropy_coeff --seed=$seed --num-attributes 4 --num-values 4 --vocab-size 100 --max-len 10 --num-senders 2 --num-receivers 2"
-		sbatch --job-name "bs_$job_id" --ntasks 8 --time 24:00:00 --output ${output_folder} run.sh $baseline_args
+		baseline_args="--sender-entropy-coeff=$entropy_coeff --receiver-entropy-coeff=$entropy_coeff --seed=$seed --num-attributes 4 --num-values 4 --vocab-size 100 --max-len 10" 
+		#--check_val_every_n_epoch 500"
+		# IF 2/100: increase log interval!!
+		sbatch --job-name "bs_$job_id" --ntasks 4 --time 24:00:00 --output ${output_folder} run.sh $baseline_args
 	    	# CRs
 		output_folder=out/train_cr_${job_id}.out
-		cr_args=$baseline_args" --noise=0.1 --clarification-requests"
-		sbatch --job-name "cr_$job_id" --ntasks 8 --time 24:00:00 --output ${output_folder} run.sh $cr_args
+		cr_args=$baseline_args" --noise=0.1 --feedback"
+		sbatch --job-name "cr_$job_id" --ntasks 4 --time 24:00:00 --output ${output_folder} run.sh $cr_args
 		job_id=$((job_id+1))
 	done
 done
