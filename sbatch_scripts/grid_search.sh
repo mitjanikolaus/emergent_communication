b192adacb25bf6bbb43b3b75043f4c545ca1d849
Mitja Nikolaus <mitja.nikolaus@posteo.de>
2022-09-24 08:57:08 +0200

    Update grid search

--- sbatch_scripts/grid_search.sh
+++ sbatch_scripts/grid_search.sh
@@ -9,15 +9,13 @@ do
 	for seed in 1 2 3 4 5
 	do
 		#Baseline
-		output_folder=out/train_baseline_${job_id}.out
-		baseline_args="--sender-entropy-coeff=$entropy_coeff --receiver-entropy-coeff=$entropy_coeff --seed=$seed --num-attributes 4 --num-values 4 --vocab-size 100 --max-len 10" 
-		#--check_val_every_n_epoch 500"
-		# IF 2/100: increase log interval!!
-		sbatch --job-name "bs_$job_id" --ntasks 4 --time 24:00:00 --output ${output_folder} run.sh $baseline_args
-	    	# CRs
-		output_folder=out/train_cr_${job_id}.out
-		cr_args=$baseline_args" --noise=0.1 --feedback"
-		sbatch --job-name "cr_$job_id" --ntasks 4 --time 24:00:00 --output ${output_folder} run.sh $cr_args
+		output_folder=out/train_bs_${job_id}.out
+		baseline_args="--sender-entropy-coeff=$entropy_coeff --receiver-entropy-coeff=$entropy_coeff --seed=$seed --num-attributes 4 --num-values 4 --vocab-size 100 --max-len 10 --check_val_every_n_epoch 500"
+		sbatch --job-name "bs_$job_id" --ntasks 2 --time 24:00:00 --output ${output_folder} run.sh $baseline_args
+	    	# Feedback
+		output_folder=out/train_fb_${job_id}.out
+		fb_args=$baseline_args" --noise=0.1 --feedback --length-cost 0.001" #TODO length cost?!
+		sbatch --job-name "fb_$job_id" --ntasks 2 --time 24:00:00 --output ${output_folder} run.sh $fb_args
 		job_id=$((job_id+1))
 	done
 done
