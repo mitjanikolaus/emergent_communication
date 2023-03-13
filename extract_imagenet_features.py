8ab884371b27f0b2a2317c5f8335a229bb961e43
Mitja Nikolaus <mitja.nikolaus@posteo.de>
2023-03-13 16:57:05 +0100

    Add extract imagent features

---
+++ extract_imagenet_features.py
@@ -0,0 +1,76 @@
+import argparse
+
+import h5py
+import numpy as np
+import os
+
+import torch
+from fiftyone.zoo import load_zoo_dataset
+from PIL import Image as PIL_Image
+from h5py import string_dtype
+from torch import nn
+
+from torchvision.models import resnet50, ResNet50_Weights
+from torchvision.transforms import transforms, InterpolationMode
+from tqdm import tqdm
+
+from utils import RESNET_IMG_FEATS_DIM, H5_IDS_KEY, DATA_DIR_IMAGENET
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+BATCH_SIZE = 100
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--source-dir", type=str)
+    args = parser.parse_args()
+
+    for split in ["train", "validation"]:
+        ds = load_zoo_dataset("imagenet-2012", split=split, max_samples=1000, source_dir=args.source_dir)
+
+        with h5py.File(os.path.join(DATA_DIR_IMAGENET, f"{split}_features.hdf5"), 'w') as h5_db:
+            resnet = resnet50(weights=ResNet50_Weights.DEFAULT)
+            modules = list(resnet.children())[:-1]
+            model = nn.Sequential(*modules)
+            for p in model.parameters():
+                p.requires_grad = False
+            model = model.to(device)
+
+            normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                             std=[0.229, 0.224, 0.225])
+
+            preprocessing = transforms.Compose([
+                transforms.Resize(256, interpolation=InterpolationMode.BICUBIC),
+                transforms.CenterCrop(224),
+                transforms.ToTensor(),
+                normalize,
+            ])
+
+            all_ids = []
+            ids_batch = []
+            images_batch = []
+            for sample in tqdm(ds):
+                img = PIL_Image.open(sample.filepath)
+                images_batch.append(img)
+                ids_batch.append(sample.id)
+                all_ids.append(sample.id)
+
+                if len(images_batch) == BATCH_SIZE:
+                    images = [preprocessing(img) for img in images_batch]
+
+                    images = torch.stack(images).to(device)
+
+                    feats = model(images).squeeze().cpu().numpy()
+
+                    for id, feat in zip(ids_batch, feats):
+                        h5_features = h5_db.create_dataset(id, RESNET_IMG_FEATS_DIM, dtype=np.float32)
+
+                        h5_features[:] = feat
+
+                    images_batch = []
+                    ids_batch = []
+
+            h5_ids = h5_db.create_dataset(H5_IDS_KEY, len(all_ids), dtype=string_dtype())
+            h5_ids[:] = all_ids
