348cd93ddf65f25f02c27cb0d3cb7b0370e1441f
Mitja Nikolaus <mitja.nikolaus@posteo.de>
2023-03-15 09:58:04 +0100

    Update sbatch scripts

--- sbatch_scripts/grid_search_guesswhat.sh
+++ sbatch_scripts/grid_search_guesswhat.sh
@@ -4,35 +4,32 @@ set -e
 
 job_id=0
 
-for entropy_coeff in 0.01 0.1 0.3 #0.5 # 0.1 0.3 0.5 0.7 #0.9 0.01 0.05
+for entropy_coeff in 0.01 #0.001 0.01 0.1 #0.01 0.05 0.2 #0.001 #0.1 # 0.01 0.3 #0.5 # 0.1 0.3 0.5 0.7 #0.9 0.01 0.05
 do
-	for seed in 2 #3 #4 5 #6 7 8 9 10 #TODO
+	for seed in 2 3 #4 5 #6 7 8 9 10 #TODO
 	do
-		num_attributes=4
-		num_values=100
 		num_senders=1
 		num_receivers=1
 		vocab_size=20
-		max_len=6
-		test_set_size=-1
+		max_len=2
 		patience=100 # TODO
 		noise=0.0 #TODO
 		length_cost=0 #0.001 #TODO
-		training_time="00:24:00:00"
-		job_time="36:00:00"
+		training_time="00:08:00:00"
+		job_time="10:00:00"
 		vocab_size_feedback=20 #100 #TODO
-		partition=besteffort #gpu
+		partition=gpu #TODO
 
-		baseline_args="--sender-entropy-coeff=$entropy_coeff --receiver-entropy-coeff=$entropy_coeff --seed=$seed --num-attributes=$num_attributes --num-values=$num_values --num-senders=$num_senders --num-receivers=$num_receivers --patience=$patience --vocab-size=$vocab_size --max-len=$max_len --check_val_every_n_epoch 10 --num-workers 2 --max_time=$training_time --sender-layer-norm --receiver-layer-norm --log-topsim-on-validation --test-set-size=$test_set_size --discrimination-game --discrimination-num-objects 5 --hard-distractors" # TODO --stochastic-receiver
+		baseline_args="--baseline-type 'mean' --precision=16 --accelerator 'gpu' --sender-entropy-coeff=$entropy_coeff --receiver-entropy-coeff=$entropy_coeff --seed=$seed --num-senders=$num_senders --num-receivers=$num_receivers --patience=$patience --vocab-size=$vocab_size --max-len=$max_len --check_val_every_n_epoch 10 --num-workers 4 --max_time=$training_time --sender-layer-norm --receiver-layer-norm --guesswhat --discrimination-num-objects 2 --sender-embed-dim 10 --receiver-embed-dim 10"
 
 		#Baseline
 		output_folder=out_gpu/train_bs_${job_id}.out
-		sbatch --job-name "bs_$job_id" --ntasks 2 --time=$job_time --output ${output_folder} --partition=$partition run.sh $baseline_args # --gres gpu:1 #TODO
+		sbatch --job-name "bs_$job_id" --ntasks 2 --time=$job_time --output ${output_folder} --partition=$partition --gres gpu:1 run.sh $baseline_args
 
                 #Reset
                 output_folder=out_gpu/train_reset_${job_id}.out
                 reset_args="$baseline_args --reset-parameters --reset-parameters-interval=1"
-                #sbatch --job-name "reset_$job_id" --ntasks 2 --time=$job_time --output ${output_folder} --partition=$partition run.sh $reset_args # --gres gpu:1 #TODO
+                #sbatch --job-name "reset_$job_id" --ntasks 2 --time=$job_time --output ${output_folder} --partition=$partition run.sh $reset_args
 
 	    	
 		# Baseline + noise
@@ -48,7 +45,7 @@ do
 		# Feedback
                 output_folder=out_gpu/train_fb_${job_id}.out
                 fb_args="$baseline_args --noise=$noise --feedback --vocab-size-feedback=$vocab_size_feedback"
-               # sbatch --job-name "fb_$job_id" --ntasks 2 --time=$job_time --output ${output_folder} --partition=$partition run.sh $fb_args
+                sbatch --job-name "fb_$job_id" --ntasks 2 --time=$job_time --output ${output_folder} --partition=$partition --gres gpu:1 run.sh $fb_args
 
 
 
