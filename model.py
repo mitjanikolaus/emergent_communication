c784545edcabf53466bd8f596fcd78b2ac2145ec
Mitja Nikolaus <mitja.nikolaus@posteo.de>
2023-09-25 14:50:48 +0200

    move to cpu before pickling

--- model.py
+++ model.py
@@ -884,7 +884,8 @@ class SignalingGameModule(pl.LightningModule):
             self.log("test_acc_no_noise", test_acc_no_noise, add_dataloader_idx=False)
 
         path = os.path.join(self.logger.log_dir, "results.pickle")
-        pickle.dump(self.trainer.logged_metrics, open(path, "wb"))
+        metrics = {n: v.cpu() for n, v in self.trainer.logged_metrics.items()}
+        pickle.dump(metrics, open(path, "wb"))
 
     def analyze_language(self, messages, meanings, is_best_checkpoint=False):
         num_unique_messages = len(messages.unique(dim=0))
