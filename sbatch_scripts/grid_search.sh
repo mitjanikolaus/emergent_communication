1647f1615d99b7fbe75777bee909dee5431d2eba
Mitja Nikolaus <mitja.nikolaus@posteo.de>
2024-02-23 14:22:17 +0100

    cleanup

--- sbatch_scripts/grid_search.sh
+++ sbatch_scripts/grid_search.sh
@@ -4,25 +4,25 @@ set -e
 
 job_id=0
 
-for entropy_coeff in 0.01 #0.001 0.01 0.1 #0.01 0.05 0.2 #0.001 #0.1 # 0.01 0.3 #0.5 # 0.1 0.3 0.5 0.7 #0.9 0.01 0.05
+for entropy_coeff in 0.01
 do
-	for seed in 2 #3 #4 5 #6 7 8 9 10
+	for seed in 1
 	do
 		num_senders=1
 		num_receivers=1
 		num_attributes=4
 		num_values=100
-		vocab_size=21
-		max_len=5
+		vocab_size=2
+		max_len=10
 		patience=100 
 		noise=0.1
-		length_cost=0.1
+		length_cost=0
 		training_time="00:08:00:00"
 		job_time="10:00:00"
 		vocab_size_feedback=2 #100
 		discrimination_num_objects=5
 
-		baseline_args="--num-attributes $num_attributes --num-values $num_values --baseline-type 'mean' --precision=16 --accelerator 'gpu' --sender-entropy-coeff=$entropy_coeff --receiver-entropy-coeff=$entropy_coeff --seed=$seed --num-senders=$num_senders --num-receivers=$num_receivers --patience=$patience --vocab-size=$vocab_size --max-len=$max_len --num-workers 4 --max_time=$training_time --sender-layer-norm --receiver-layer-norm --discrimination-num-objects $discrimination_num_objects --sender-embed-dim 10 --receiver-embed-dim 32 --length-cost $length_cost --noise=$noise --receiver-output-attention" # hard-distractors
+		baseline_args="--num-attributes $num_attributes --num-values $num_values --baseline-type 'mean' --precision=16 --accelerator 'gpu' --sender-entropy-coeff=$entropy_coeff --receiver-entropy-coeff=$entropy_coeff --seed=$seed --num-senders=$num_senders --num-receivers=$num_receivers --patience=$patience --vocab-size=$vocab_size --max-len=$max_len --num-workers 4 --max_time=$training_time --sender-layer-norm --receiver-layer-norm --discrimination-num-objects $discrimination_num_objects --length-cost $length_cost --noise=$noise --receiver-output-attention" # hard-distractors
 
 		#Baseline
 		#output_folder=out_gpu/train_bs_${job_id}.out
